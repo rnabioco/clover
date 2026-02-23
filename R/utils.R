0ffb2084d1cd5b49803ec6e3869aa4fdbe83ee9d
Jay Hesselberth <jay.hesselberth@gmail.com>
2026-02-23 07:11:20 -0700

    Export dna_to_rna_anticodon() to fix vignette build error

--- R/utils.R
+++ R/utils.R
@@ -10,9 +10,18 @@ clover_example <- function(path) {
   system.file("extdata", path, package = "clover", mustWork = TRUE)
 }
 
-# Convert DNA anticodon to RNA in tRNA name strings.
-#
-# e.g., "tRNA-Glu-TTC-1-1" -> "tRNA-Glu-UUC-1-1"
+#' Convert DNA anticodon to RNA in tRNA name strings
+#'
+#' Replaces T with U in the anticodon portion of tRNA identifiers,
+#' e.g., `"tRNA-Glu-TTC-1-1"` becomes `"tRNA-Glu-UUC-1-1"`.
+#'
+#' @param x Character vector of tRNA name strings.
+#'
+#' @return Character vector with DNA anticodons converted to RNA.
+#'
+#' @export
+#' @examples
+#' dna_to_rna_anticodon("tRNA-Glu-TTC-1-1")
 dna_to_rna_anticodon <- function(x) {
   parts <- stringr::str_match(x, "^(tRNA-[A-Za-z0-9]+-)(\\w+)(-.*)")
   ifelse(
